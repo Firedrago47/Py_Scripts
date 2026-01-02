38d1c4aad37accfa49eb2b1e92a65df0dfad0262
Deep <deepanrajpmk@gmail.com>
2026-01-03 00:51:49 +0530

    feat:mac addr changer

--- macadd_changer.py
+++ macadd_changer.py
@@ -1,3 +1,11 @@
 import subprocess
 
-subprocess.call("ip addr",shell=True)
+
+interface = input("Enter the Interface:")
+subprocess.call("ip a show dev " + interface,shell=True)
+macaddr = input("Enter the Address:")
+subprocess.call("ip link set dev " + interface + " down",shell=True)
+subprocess.call("ip link set dev " + interface + " address " + macaddr,shell=True)
+subprocess.call("ip link set " + interface + " up",shell=True)
+
+
