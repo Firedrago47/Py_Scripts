a6377081e63ff5500c51689013450503eaa6ebf2
Deep <deepanrajpmk@gmail.com>
2026-01-03 01:01:04 +0530

    feat:prevent prompt injections by using list

--- macadd_changer.py
+++ macadd_changer.py
@@ -2,10 +2,17 @@ import subprocess
 
 
 interface = input("Enter the Interface:")
-subprocess.call("ip a show dev " + interface,shell=True)
+#subprocess.call("ip a show dev " + interface,shell=True)
+#subprocess.call("ip link set dev " + interface + " down",shell)
+#subprocess.call("ip link set dev " + interface + " address " + macaddr,shell=True)
+#subprocess.call("ip link set " + interface + " up",shell=True)
+
+subprocess.call(["ip","a","show","dev",interface])
+
 macaddr = input("Enter the Address:")
-subprocess.call("ip link set dev " + interface + " down",shell=True)
-subprocess.call("ip link set dev " + interface + " address " + macaddr,shell=True)
-subprocess.call("ip link set " + interface + " up",shell=True)
 
+subprocess.call(["ip","link","set","dev",interface,"down"])
 
+subprocess.call(["ip","link","set","dev",interface,"address",macaddr])
+
+subprocess.call(["ip","link","set",interface,"up"])
