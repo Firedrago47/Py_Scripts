8be0ff69f8c5082e85d9a9841c2fbe6e202767d6
Deep <deepanrajpmk@gmail.com>
2026-01-04 00:13:28 +0530

    feat:options added and func used

--- macadd_changer.py
+++ macadd_changer.py
@@ -1,18 +1,23 @@
 import subprocess
+import optparse
+
+def get_options():
+    parse = optparse.OptionParser()
+    parse.add_option("-i","--interface",dest="interface",help="Used to select interface")
+    parse.add_option("-m","--mac",dest="macaddr",help="Used to select New mac address")
+    (option,argument) = parse.parse_args()
+    if not option.interface:
+        parse.error("Interface not specified")
+    elif not option.macaddr:
+        parse.error("Mac Address not specified")
+    return option
 
 
-interface = input("Enter the Interface:")
-#subprocess.call("ip a show dev " + interface,shell=True)
-#subprocess.call("ip link set dev " + interface + " down",shell)
-#subprocess.call("ip link set dev " + interface + " address " + macaddr,shell=True)
-#subprocess.call("ip link set " + interface + " up",shell=True)
+def mac_change(interface,macaddr):
+    print("Mac Address of "+ interface + " Changing to "+ macaddr)
+    subprocess.call(["ip","link","set","dev",interface,"down"])
+    subprocess.call(["ip","link","set","dev",interface,"address",macaddr])
+    subprocess.call(["ip","link","set",interface,"up"])
 
-subprocess.call(["ip","a","show","dev",interface])
-
-macaddr = input("Enter the Address:")
-
-subprocess.call(["ip","link","set","dev",interface,"down"])
-
-subprocess.call(["ip","link","set","dev",interface,"address",macaddr])
-
-subprocess.call(["ip","link","set",interface,"up"])
+option = get_options()
+mac_change(option.interface,option.macaddr)
