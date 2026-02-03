d225c1d40010c503bd60c69104ada402943f54ae
Deep <deepanrajpmk@gmail.com>
2026-02-03 17:50:21 +0530

    feat:check subnet inside parent range

--- nw-validator.py
+++ nw-validator.py
@@ -28,6 +28,17 @@ def validate_all_subnet(subnets):
     return valid_networks
 
 
+def validate_subnets_of_parent(parent, subnets):
+    for subnet in subnets:
+        net = subnet.subnet_of(parent)
+        if not net:
+            print(f"Invalid,{subnet} not a subnet of {parent} Outside the range")
+            return False
+        else:
+            print(f"Valid,{subnet} is inside parent range")
+    return True
+
+
 if __name__ == "__main__":
     path = load_plan("ip_plan.json")
 
@@ -35,8 +46,14 @@ if __name__ == "__main__":
 
     if not parent:
         print("Invalid parent range")
-    else:
-        print("Parent range is valid: ", parent)
+        exit(1)
 
     subnets = validate_all_subnet(path["subnets"])
-    print(subnets)
+
+    print("\n Checking subnets are inside the parent range...\n")
+
+    if not validate_subnets_of_parent(parent, subnets):
+        print("Invalid,Check the Ip plan")
+        exit(1)
+
+    print("\n All subnets are inside the parent range\n")
